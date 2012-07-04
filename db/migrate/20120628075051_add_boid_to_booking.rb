f022c36d65d2be0600096a62dd1a2e620bc211f4
yuanzhuang <zyuan@rbcon.com>
2012-07-04 17:33:31 +0800

    Add comment for the boid. BOID is Booking Operation ID. Some booking items may have the same BOID when the bookings is sumbitted together. They will share the same recurring info and edit,delete operations.

--- db/migrate/20120628075051_add_boid_to_booking.rb
+++ db/migrate/20120628075051_add_boid_to_booking.rb
@@ -1,5 +1,5 @@
 class AddBoidToBooking < ActiveRecord::Migration
   def change
-    add_column :bookings, :boid, :string
+    add_column :bookings, :boid, :string # boid = Booking Operation ID
   end
 end
