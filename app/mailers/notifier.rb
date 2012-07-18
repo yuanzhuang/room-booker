bd59438c977eaca5a659ad0bcfbf697c51094040
yuanzhuang <zyuan@rbcon.com>
2012-07-18 10:50:40 +0800

    modify the code of sending mail with attachment.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -6,18 +6,16 @@ class Notifier < ActionMailer::Base
 
   # send a notification mail to the organizer and all invitees if one booking done
   def notification_mail(booking)
-
-
-
     filename = "tmpfile/"+booking.guid
     ics_content = generate_ics(booking)
     ics_file = File.new( filename, "w")
     ics_file.write(ics_content.to_s)
     ics_file.close
 
-    attachments['invite.ics'] = File.read(filename)
+    mail_addr = User.find(booking.user_id).username
+    attachments['invite.ics'] = {:content => File.read(filename), :mime_type => "text/calendar"}
 
-    mail(:to => "whywhy36@sina.com", :subject => "Test SendGrid Service", :template_path => "notifier", :template_name => "another" )
+    mail(:to => mail_addr, :subject => booking.description, :template_path => "notifier", :template_name => "content" )
 
   end
 
