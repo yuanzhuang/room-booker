9197252aa9615b549e51b344fd8919078bc8f73d
yuanzhuang <zyuan@rbcon.com>
2012-07-24 21:34:38 +0800

    remove bugs in search mode booking

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -18,25 +18,59 @@ class SearchController < ApplicationController
 
     # device requirements
 
+    logger.info "Search available rooms with the arguments :"+@required_booking.to_s
 
-    devices_requirement = 0
+    @devices_requirement = 0
 
     if !params[:projector].nil?
-      devices_requirement |= 1
+      @devices_requirement |= 1
     end
 
     if !params[:telephone].nil?
-      devices_requirement |= 2
+      @devices_requirement |= 2
     end
 
     # recurring requirements
     @required_booking.recurring = params[:recurring]
 
+
+    # more recurring day information
+    @recurring_days = Array.new
+
+    if !params[:recurringday1].nil?
+      @recurring_days <<  0
+    end
+
+    if !params[:recurringday2].nil?
+      @recurring_days << 1
+    end
+
+    if !params[:recurringday3].nil?
+      @recurring_days << 2
+    end
+
+    if !params[:recurringday4].nil?
+      @recurring_days << 3
+    end
+
+    if !params[:recurringday5].nil?
+      @recurring_days << 4
+    end
+
+    if !params[:recurringday6].nil?
+      @recurring_days << 5
+    end
+
+    if !params[:recurringday7].nil?
+      @recurring_days << 6
+    end
+
+
     # capacity requirement
-    capacity_requirement = params[:capacity]
+    @capacity_requirement = params[:capacity]
 
 
-    @rooms = get_available_rooms @required_booking, devices_requirement, capacity_requirement
+    @rooms = get_available_rooms @required_booking, @devices_requirement, @capacity_requirement , @recurring_days
 
     respond_to do |format|
       format.html
@@ -53,6 +87,52 @@ class SearchController < ApplicationController
     @room = Room.find(params[:room])
     #@room_name = build_room_details room
     @room_name = @room.name
+
+    @recurring = params[:recurring]
+
+    @devices_requirement = 0
+
+    if !params[:projector].nil?
+      @devices_requirement |= 1
+    end
+
+    if !params[:telephone].nil?
+      @devices_requirement |= 2
+    end
+
+
+    # more recurring day information
+    @recurring_days = Array.new
+
+    if !params[:recurringday1].nil?
+      @recurring_days <<  0
+    end
+
+    if !params[:recurringday2].nil?
+      @recurring_days << 1
+    end
+
+    if !params[:recurringday3].nil?
+      @recurring_days << 2
+    end
+
+    if !params[:recurringday4].nil?
+      @recurring_days << 3
+    end
+
+    if !params[:recurringday5].nil?
+      @recurring_days << 4
+    end
+
+    if !params[:recurringday6].nil?
+      @recurring_days << 5
+    end
+
+    if !params[:recurringday7].nil?
+      @recurring_days << 6
+    end
+
+
 
     respond_to do |format|
       format.html
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,13 +2,13 @@ module SearchHelper
 
   include BookingsHelper
 
-  def get_available_rooms(booking, devices, capacity)
+  def get_available_rooms(booking, devices, capacity, recurring_days)
 
     available_room = Array.new
     rooms = get_all_rooms
 
     rooms.each do |room|
-      if !check_conflicts_with_room booking,room.id
+      if !check_conflicts_with_room booking,room,devices,capacity,recurring_days
           available_room << room
       end
     end
@@ -18,14 +18,49 @@ module SearchHelper
   end
 
   # return true => conflict, false => no conflict
-  def check_conflicts_with_room(booking, roomid)
+  def check_conflicts_with_room(booking, room, devices, capacity, recurring_days)
 
-    bookings = Booking.find_all_by_room_id( roomid )
+    if devices != room.devices
+      return true
+    end
+
+    logger.info "capacity is #{capacity.to_i} and rooms capacity is #{room.capacity}"
+    if !capacity.nil?
+
+      if capacity.to_i > room.capacity
+        return true
+      end
+    end
+
+    dates = split_booking booking.startdate,recurring_days
+
+    splited_bookings = Array.new
+    dates.each do |date|
+      tmp_booking = Booking.new
+      tmp_booking.enddate = booking.enddate
+      tmp_booking.starttime = booking.starttime
+      tmp_booking.endtime = booking.endtime
+      tmp_booking.recurring = booking.recurring
+      tmp_booking.startdate = date
+      splited_bookings << tmp_booking
+    end
+
+    bookings = Booking.find_all_by_room_id room.id
+
+
 
     bookings.each do |booking_at_room|
+=begin
       if check_conflicts_with_specific booking,booking_at_room
         return true
       end
+=end
+      splited_bookings.each do |splited_booking|
+        if check_conflicts_with_specific splited_booking,booking_at_room
+          return true
+        end
+      end
+
     end
 
     return false
